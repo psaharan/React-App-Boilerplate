14752e355c7d0a1ed4cbc51a4205a3706fb8a722
Pankaj Saharan <pankaj.saharan@gmail.com>
2016-12-28 17:56:14 +0200

    Initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var Navigation = require('Navigation');
+
+var Main = (props) => {
+  return (<div>
+    <div>
+      <div>
+          <Navigation />
+          {props.children}
+      </div>
+    </div>
+  </div>
+  )
+};
+
+module.exports = Main;
